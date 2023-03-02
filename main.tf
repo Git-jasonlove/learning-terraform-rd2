7716421a0f39e7c851e652133bd94dd796dcd62c
Jason Love <jason.love@omnicell.com>
2023-03-02 17:47:11 -0600

    corrected typo

--- main.tf
+++ main.tf
@@ -57,7 +57,7 @@ resource "aws_security_group_rule" "blog_http_in" {
 }
 
 resource "aws_security_group_rule" "blog_everything_out" {
-  type        = "engress"
+  type        = "egress"
   from_port   = 0
   to_port     = 0
   protocol    = "tcp-1"
